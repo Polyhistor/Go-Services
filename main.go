c6c41ffd8cba0953c9ab1b3036297cbfc5d2c2a2
Polyhistor <pouya.ataei.7@gmail.com>
2021-03-14 19:35:05 +1300

    added route handlers, and two templates

--- main.go
+++ main.go
@@ -7,22 +7,6 @@ import (
 
 var portNumber = ":8080"
 
-// Home is the homge page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the home page")
-}
-
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	sum := addValues(2, 2)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
-}
-
-// addValues adds two integers and returns the sum
-func addValues(x, y int) int {
-	return x + y
-}
-
 // main is the main application function
 func main() {
 	http.HandleFunc("/", Home)
