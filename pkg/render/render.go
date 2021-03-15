4a83a8eb55a6cc44d7c2c20e1cf668526dfc4598
Polyhistor <pouya.ataei.7@gmail.com>
2021-03-15 22:19:43 +1300

    added render template function

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -16,18 +16,44 @@ func RenderTemplate (w http.ResponseWriter, tmpl string) {
 	}
 }
 
+var functions = template.FuncMap{
 
-func RenderTemplateTest(w http.ResponseWriter, tmpl string) error {
+}
+
+
+func RenderTemplateTest(w http.ResponseWriter, tmpl string) (map[string]*template.Template, error) {
 	myCache := map[string]*template.Template{}
 	pages, err := filepath.Glob("./templates/*.page.tmpl")
 
-	if err !=nil {
-		return err
+	if err != nil {
+		return myCache, err
 	}
 
 	for _, page := range pages {
 		name := filepath.Base(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 
-		ts, err := template.New(name)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+
+			myCache[name] = ts
+			return myCache, ts
+		}
+
+		return myCache, nil
+
 	}
 }
